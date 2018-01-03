ef91f3da7c333649678ac992cb2de8676ec6ccb4
jackson <jackson@swordfish.co.za>
2018-01-03 23:31:37 +0200

    qualify subscriber
    add extra console logging

--- observer.py
+++ observer.py
@@ -1,4 +1,4 @@
-from subscriber import Subscriber
+from observer.subscriber import Subscriber
 
 
 class Publisher:
@@ -7,6 +7,7 @@ class Publisher:
         # list of subscriber objects
         self.subscribers = {events: dict()
                             for events in events}
+        self.count = 0
 
     def get_subscribers(self, event):
         return self.subscribers[event]
@@ -17,11 +18,14 @@ class Publisher:
             if not callback:
                 callback = getattr(subscriber, 'update')
             self.get_subscribers(event)[subscriber] = callback
+            self.count+=1
+            print('Added subscriber {count}'.format(count=self.count))
         else:
             print('This object in not an instance of Subscriber')
 
     def unsubscribe(self, event, subscriber):
         del self.get_subscribers(event)[subscriber]
+        self.count = - 1
 
     def dispatch(self, event, message):
         for subscriber, callback in self.get_subscribers(event).items():
